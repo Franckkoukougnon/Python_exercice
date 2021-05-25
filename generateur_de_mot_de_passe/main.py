bb1ada6843e4d934a439624ddac778e716b02828
franckkoukougnon <franckkoukougnon@yahoo.fr>
2021-05-25 19:08:11 +0200

    new application

---
+++ generateur_de_mot_de_passe/main.py
@@ -0,0 +1,7 @@
+import random
+
+longpasswd = int(input("donner la longueur du mot de passe "))
+choice_letter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&()*+,-./:;<=>?@[]^_`{|}~'
+
+password="".join(random.sample(choice_letter, longpasswd))
+print(password)
