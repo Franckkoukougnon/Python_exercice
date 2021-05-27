d8640fb2e2b53d2ce5f0b0663f526251a570f41a
franckkoukougnon <franckkoukougnon@yahoo.fr>
2021-05-27 02:00:31 +0200

    modifiaction

--- generateur_de_mot_de_passe/main.py
+++ generateur_de_mot_de_passe/main.py
@@ -3,5 +3,6 @@ import random
 longpasswd = int(input("donner la longueur du mot de passe "))
 choice_letter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&()*+,-./:;<=>?@[]^_`{|}~'
 
-password="".join(random.sample(choice_letter, longpasswd))
-print("vous avez choisi un password avec " + longpasswd + " mots , votre mot de passe sera alors " + password )
+password ="".join(random.sample(choice_letter, longpasswd))
+print(longpasswd)
+print("votre mot de passe est "+ password)
