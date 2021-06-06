48043cdba6ccf9db4b3d55a11caff0cae860632c
franckkoukougnon <franckkoukougnon@yahoo.fr>
2021-06-06 19:30:50 +0200

    recap

---
+++ Classe_python/main.py
@@ -0,0 +1,18 @@
+class Voiture:
+    def __init__(self, marque):
+        self.marque = marque
+
+voiture_01 = Voiture("Nissan")
+
+voiture_02 = Voiture("BMW")
+
+print(voiture_02.marque)
+
+print(voiture_01.marque)
+
+
+
+
+
+
+
